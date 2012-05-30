f03ac8c5e67116bf35683b36cd71b656d610d9e6
Jeff Buchbinder <jeff@ourexchange.net>
2012-05-30 15:07:47 -0400

    Formatting.

--- test/main.go
+++ test/main.go
@@ -6,13 +6,12 @@ import "log"
 import "net"
 
 func main() {
-	//flag.Parse()
 	log.Printf("Initializing test")
 	gIP := net.IPv4(127, 0, 0, 1)
 	gangliaPort := 1234
-	
-	gm := gmetric.Gmetric{ gIP, gangliaPort, "127.0.0.1", "127.0.0.1:spoof" }
-	
+
+	gm := gmetric.Gmetric{gIP, gangliaPort, "127.0.0.1", "127.0.0.1:spoof"}
+
 	for i := 0; i < 10; i++ {
 		log.Printf("Sending packet for some_metric_%d", i)
 		gm.SendMetric(fmt.Sprintf("some_metric_%d", i), "8675309", gmetric.VALUE_UNSIGNED_INT, "units", gmetric.SLOPE_BOTH, 300, 600, "GROUP")
