2768643f1a9ad1eb412340613d7578806c4361fa
Jeff Buchbinder <jeff@ourexchange.net>
2012-05-30 15:03:13 -0400

    Add simple test strap to point at gmond-debug instance on localhost.

---
+++ test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "github.com/jbuchbinder/go-gmetric/gmetric"
+import "log"
+import "net"
+
+func main() {
+	//flag.Parse()
+	log.Printf("Initializing test")
+	gIP := net.IPv4(127, 0, 0, 1)
+	gangliaPort := 1234
+	
+	gm := gmetric.Gmetric{ gIP, gangliaPort, "127.0.0.1", "127.0.0.1:spoof" }
+	
+	for i := 0; i < 10; i++ {
+		log.Printf("Sending packet for some_metric_%d", i)
+		gm.SendMetric(fmt.Sprintf("some_metric_%d", i), "8675309", gmetric.VALUE_UNSIGNED_INT, "units", gmetric.SLOPE_BOTH, 300, 600, "GROUP")
+	}
+}
